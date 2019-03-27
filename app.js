2692c9b933ec193a32fbf5f305f0ecda42881f1c
Jacek Kopecky <jacek@jacek.cz>
2019-03-27 17:33:38 +0000

    enable offline use and app installation
    fix #6 
    
    app.js loads app_worker.js enables offline through SW cache.
    
    manifest.json (with two images) makes tool installable on phone/desktop
    
    Co-Authored-By: Rich Boakes <rjb@port.ac.uk>

---
+++ app.js
@@ -0,0 +1,15 @@
+// if the browser supports service workers
+// install one.
+if (navigator.serviceWorker) {
+    console.log("ServiceWorker: support detected.");
+    async function registerServiceWorker() {
+        console.log("ServiceWorker: registering.");
+        try {
+            const reg = await navigator.serviceWorker.register('./app_worker.js');
+            console.log("ServiceWorker: registered", reg);
+        } catch (e) {
+            console.error("Service Worker failed.  Falling back to online-only.", e);
+        }
+    }
+    window.addEventListener("load", registerServiceWorker);
+};
