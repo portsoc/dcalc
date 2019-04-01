b32be773810d578e3102a52554aaac8067e89bfb
Rich Boakes <rich@boakes.org>
2019-04-01 19:51:16 +0100

    Don't save data if not valid. Enforce 0-100 in number fields. Give useful error messages if outside 0-100 range.

---
+++ js/validity.js
@@ -0,0 +1,42 @@
+const messages = {
+    "max100": "100 is the maximum grade.",
+    "min0": "Zero (0) is the lowest grade."
+};
+
+let numbers, all;
+
+function init() {
+
+    numbers = document.querySelectorAll('input[type="number"]');
+    all = document.querySelectorAll('input');
+
+    for (const num of numbers) {
+        num.addEventListener('input', checkNumberValidity);
+    }
+
+}
+
+function report(elem, msg) {
+    all.forEach(e => e.setCustomValidity(""));
+    elem.setCustomValidity(msg);
+    elem.reportValidity();
+}
+
+function checkNumberValidity(e) {
+    if (e.target.validity.rangeOverflow) {
+        e.target.value = Math.min(e.target.max, e.target.value);
+        report(e.target, messages.max100);
+        e.preventDefault();
+        return false;
+    }
+    if (e.target.validity.rangeUnderflow) {
+        e.target.value = Math.max(e.target.min, e.target.value);
+        report(e.target, messages.min0);
+        e.preventDefault();
+        return false;
+    }
+    report(e.target, "");
+}
+
+
+window.addEventListener('load', init);
