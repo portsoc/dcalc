3b7d7e3d7c325c205750432ccbbc32f3edc6a58e
Jacek Kopecky <jacek@jacek.cz>
2021-09-13 10:55:00 +0100

    :shirt:

--- app_worker.js
+++ app_worker.js
@@ -9,7 +9,7 @@ const cacheable = [
   './js/ui.js',
   './style.css',
   './img/192.png',
-  './img/512.png'
+  './img/512.png',
 ];
 
 /* Invoke the default fetch capability to
@@ -23,16 +23,16 @@ async function updateCache(request) {
 }
 
 /* Retrieve a requested resource from the cache
- * or return a resolved promise if its not there.  
+ * or return a resolved promise if its not there.
  */
 async function handleFetch(request) {
   const c = await caches.open(CACHE);
   const cachedCopy = await c.match(request);
-  return cachedCopy || Promise.reject('no-match');
+  return cachedCopy || Promise.reject(new Error('no-match'));
 }
 
 /* All GET requests are first served from
- * the cache, before an attempt is made to 
+ * the cache, before an attempt is made to
  * update the cache.
  */
 function interceptFetch(evt) {
@@ -43,11 +43,11 @@ function interceptFetch(evt) {
 /* Installing the service worker involves
  * preparing an populating the cache, here.
  */
-async function prepareCache(evt) {
-  console.log("Cache: preparing");  
+async function prepareCache() {
+  console.log('Cache: preparing');
   const c = await caches.open(CACHE);
   c.addAll(cacheable);
-  console.log("Cache: prepared");  
+  console.log('Cache: prepared');
 }
 
 // install the event listsner so it can run in the background.
