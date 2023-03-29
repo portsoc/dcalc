8cd2cf936d5d43b9ae0290de80f8994ea6edb660
Mar <up2079248@myport.ac.uk>
2023-03-29 13:10:46 +0100

    inital commit

---
+++ svr.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import express from 'express';
+import getStudent from './getStudent.mjs';
+
+const app = express();
+app.use(express.static('client'));
+
+// a request for retrieving student's data
+app.get('/student/:studentID', async (req, res) => {
+  const student = await getStudent(req.params.studentID.slice(2));
+
+  // check if the student's data actually exists
+  if (student === null) {
+    res.json(null);
+  } else {
+    res.json("Good request");
+  }
+});
+
+
+app.listen(5500);
