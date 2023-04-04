9384e5339f10429f4de2a0c98e21a3e00cd2e6ed
NikBit101 <n.cern.03@gmail.com>
2023-04-04 13:03:58 +0100

    implemented a 'proxy' listening for any requests

---
+++ client/js/student-request.js
@@ -0,0 +1,25 @@
+function getModules(student) {
+  // student does NOT exist, display warning message and cancel request
+  if (student.studentModules === null) {
+    console.log('Student data does not exist.');
+    return null;
+  }
+
+  const sModules = student.studentModules.modules;
+  // eslint-disable-next-line no-restricted-syntax
+  for (const module in sModules) {
+    console.log(sModules[module]);
+  }
+}
+
+export default async function getStudentData(id, label) {
+  console.log(`Result: before`);
+  const route = `student/SOC/tutorial/${id.slice(2)}/`;
+  const response = await fetch(route);
+  const res = await response.json();
+  console.log(res);
+
+
+  const student = await getModules(res);
+  console.log(student);
+}
