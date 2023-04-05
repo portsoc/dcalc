0397e9ff4ba00a5922cbbb4d8e9797872b9d03e0
William Woods <up2021407@myport.ac.uk>
2023-04-05 18:18:56 +0100

    Will's changes - Altered for loops to array iteration method. Altered boolean if check method

--- client/js/student-request.js
+++ client/js/student-request.js
@@ -1,6 +1,6 @@
 function getModules(student, id, label) {
   // student does NOT exist, display warning message and cancel request
-  if (student.studentModules === null || student.studentModules === undefined) {
+  if (!student.studentModules) {
     label.textContent = `Sorry the student ${id} does not exist`;
     return null;
   }
@@ -14,31 +14,28 @@ function getModules(student, id, label) {
   
   // confirm whether module grades are from level 5 or 6
   if (is_LVLFive) {
-    for (const module in sModules) {;
+    const LEVEL_5 = [];
+    
+    Object.values(sModules).forEach((module, i) => {
       const modMarks = {
-        name: sModules[module].name,
-        marks: sModules[module].marks.agreedModuleMark
+        name: module.name,
+        marks: module.marks.agreedModuleMark
       };
       LEVEL_5.push(modMarks);
-    }
-        
-    // autofill the modules to "second year" list
-    for (let i = 0; i < LEVEL_5.length-1; i++) {
-      const module = LEVEL_5[i];
-      const modNameInput = document.querySelector(`#l5name${(i+1)}`);
-      const moduleMarksInput = document.querySelector(`#l5mark${(i+1)}output`);
-      modNameInput.value = module.name;
-      moduleMarksInput.value = module.marks;
-    }    
+  
+      const modNameInput = document.querySelector(`#l5name${i+1}`);
+      const moduleMarksInput = document.querySelector(`#l5mark${i+1}output`);
+      modNameInput.value = modMarks.name;
+      moduleMarksInput.value = modMarks.marks;
+    });
+    
+    console.log(LEVEL_5);
   }
-  console.log(LEVEL_5);
 }
 
 export default async function getStudentData(id, label) {
   const route = `student/SOC/tutorial/${id.slice(2)}/`;
   const response = await fetch(route);
   const res = await response.json();
-
-
   const student = await getModules(res, id, label);
 }
